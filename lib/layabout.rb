9d3fc17332caac12f9657a240689459985213bbf
Scott Steele <scottlsteele@gmail.com>
2014-03-25 20:59:50 -0400

    Set up basic alignment
    Note: Requires explicit dimensions...no inferences.
    
    Also, added initial test for alignment of one object with respect to
    other object. See FIXME comments in lib/layabout.rb for where to
    implement.

--- lib/layabout.rb
+++ lib/layabout.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 # layabout.rb
 require 'ostruct'
+require 'pry'
 
 module Layabout
 
@@ -12,7 +13,7 @@ module Layabout
   class Rect
     attr_reader :width, :height, :x, :y
 
-    def initialize width: 0, height: 0, x: 0, y: 0, &block
+    def initialize width: 0, height: 0, x: 0, y: 0, wrt: nil, &block
       @width = width
       @height = height
       @x = x
@@ -23,9 +24,62 @@ module Layabout
 
     def rect name=nil, opts={}, &block
       singleton_class.class_exec { attr_reader name }
+      opts = handle_wrt opts if opts[:wrt]
+      opts = handle_child opts
       out = instance_variable_set "@#{name}", Layabout.rect(opts, &block)
       @contents << out
       out
+    end
+
+    protected
+    def handle_child opts
+      alignments = %i(center left top right bottom)
+      %i(x y).each do |axis|
+        opts[axis] = handle_align axis, opts if alignments.include? opts[axis]
+      end
+      opts
+    end
+
+    def handle_align axis, opts, reference_object = self
+      dimension = case axis
+                  when :x then :width
+                  when :y then :height
+                  end
+
+      case opts[axis]
+      when :center
+        if opts[:outside]
+          #FIXME
+        else
+          (reference_object.send(dimension).to_f / 2) - (opts[dimension].to_f / 2)
+        end
+      when :left, :top
+        if opts[:outside]
+          #FIXME
+        else
+          reference_object.send axis
+        end
+      when :right, :bottom
+        if opts[:outside]
+          #FIXME
+        else
+          reference_object.send(dimension) - opts[dimension]
+        end
+      end
+    end
+
+    def handle_wrt opts
+      opts[:wrt].each do |key, align_opts|
+        ref_obj = send(key)
+        align_opts.each do |axis, alignment|
+          # We're passing in the *original* opts merged with align_opts...
+          # ...not just the current align_opts.
+          opts[axis] = handle_align axis, opts.merge(align_opts).merge(outside: true), ref_obj
+          #binding.pry
+        end
+      end
+
+      opts
     end
   end
 end
--- spec/layabout_spec.rb
+++ spec/layabout_spec.rb
@@ -32,27 +32,76 @@ describe Layabout::Rect do
   it do
     opts = { width: 10, height: 10, x: 15, y: 15 }
     r = Layabout.rect(opts)
-    opts.each {|(methd, val)| expect(r.send methd).to be(val) }
+    opts.each {|(methd, val)| expect(r.send methd).to eq(val) }
   end
 
   it do
     outer_shield = BasicObject.new
     r = Layabout.rect do
-      rect :shield, width: 5
+      rect :shield, width: 5, height: 15
       outer_shield = shield
     end
     expect(r).to respond_to(:shield)
     expect(r.shield).to be_a(Layabout::Rect)
-    expect(r.shield.width).to be(5)
+    expect(r.shield.width).to eq(5)
+    expect(r.shield.height).to eq(15)
     expect(r.shield).to be(outer_shield)
   end
 
-  it do
+  pending do
     outer_shield = BasicObject.new
-    r = Layabout.rect do
+    r = Layabout.rect width: :infer do
       rect :shield, width: 5
     end
+    expect(r.width).to eq(0)
+  end
 
+  # Let's be as explicit in our declarations as we can tolerate
+  it do
+    r = Layabout.rect width: 100, height: 200 do
+      rect :shield,
+        width: 10, height: 20,
+        x: :center, y: :center
+
+      rect :header,
+        width: 20, height: 50,
+        x: :left, y: :top
+
+      rect :footer,
+        width: 20, height: 50,
+        x: :right, y: :bottom
+    end
+
+    expect(r.shield.x).to eq(45)
+    expect(r.shield.y).to eq(90)
+
+    expect(r.header.x).to eq(0)
+    expect(r.header.y).to eq(0)
+
+    expect(r.footer.x).to eq(80)
+    expect(r.footer.y).to eq(150)
+  end
+
+  # relative layout
+  it do
+    r = Layabout.rect width: 100, height: 200 do
+      rect :shield,
+        width: 10, height: 20,
+        x: :center, y: :center
+
+      rect :charge_dexter,
+        width: 10, height: 40,
+        # The x opt of :right means charge_dexter is
+        # flush with shield on *charge_dexter's* right
+        # side...i.e., shield will be to the right
+        # of charge_dexter... charge_dexter won't be
+        # to the right of shield.
+        wrt: {shield: {x: :right,
+                       y: :center}}
+    end
+
+    expect(r.charge_dexter.x).to eq(35)
+    expect(r.charge_dexter.y).to eq(80)
   end
 end
 
@@ -76,7 +125,7 @@ rect.shield.x
 ## Sketch 2
 rect = Layabout.rect do
   rect :shield, width: 15, height: 15
-  
+
   add_above :shield,
     rect(:crest, width: 20, height: 10)
 
@@ -91,7 +140,7 @@ rect = Layabout.rect do
   # Alternate syntax
   rect :crest, width:20, height: 10
   shield.below :crest
-  
+
   # Alternate syntax
   add_above :shield,
     rect(:crest, width: 20, height: 10)
